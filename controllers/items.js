62b56e129245597d84035b69df60e5d93bf30382
PhillipCostas <Phillipcostas@gmail.com>
2024-06-10 16:04:18 -0400

    got one edit link to work, the rest break

--- controllers/items.js
+++ controllers/items.js
@@ -17,8 +17,34 @@ router.get('/show', async (req, res) => {
   });
 
 
-//   router.get('/new', async (req, res) => {
-//     res.render('items/show.ejs')
-// });
+//   router.post('/', async (req, res) => {
+//     try {
+//       console.log(req.body)
+//         const currentUser = await User.findById(req.session.user._id); 
+//         currentUser.suitcase.push(req.body);
+//         await currentUser.save()
+//         res.redirect(`/users/${currentUser._Id}/items`);
+//     } catch (error) {
+//         console.log(error);
+//         res.redirect('/');
+//     }
+// })
+
+
+  router.get('/suitcase/:itemsId/edit', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      // console.log(currentUser)
+      const suitcase = currentUser.suitcase.id(req.params.itemsId);
+      res.render('items/edit.ejs', {
+        items: suitcase,
+      });
+    } catch (error) {
+      console.log(error);
+      res.redirect('/')
+    }
+  })
+
+
 
 module.exports = router; 
